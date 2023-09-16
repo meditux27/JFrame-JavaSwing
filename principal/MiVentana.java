c27a5dc586b7da905d88abfd374aa96fb3c903cc
Miguel Angel <102642889+meditux27@users.noreply.github.com>
2023-09-16 10:50:42 -0600

    Update MiVentana.java

--- principal/MiVentana.java
+++ principal/MiVentana.java
@@ -6,8 +6,6 @@ public class MiVentana {//Modificar el nombre de la clase a su preferencia o rea
         JFrame frame = new JFrame("Mi Aplicación Swing"); // Crear un nuevo JFrame con un título
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Configurar el comportamiento al cerrar
 	frame.setLocationRelativeTo(null);//Establecer la posición inicial al ejecutar la ventana
-        // Agregar componentes, como botones o etiquetas, al panel de contenido del JFrame aquí
-
         frame.setSize(400, 300); // Establecer el tamaño inicial de la ventana
         frame.setVisible(true); // Hacer visible la ventana
         
